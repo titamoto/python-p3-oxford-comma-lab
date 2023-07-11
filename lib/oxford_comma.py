0e3eb34ae0f66234a4b684e5224b8b4b269650c4
Aleksandr <titamoto@gmail.com>
2023-07-11 12:45:08 -0700

    complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,7 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 2:
+        return " and ".join(items)
+    elif len(items) == 1:
+        return items[0]
+    else:
+        return ", ".join(items[:-1]) + ", and " + items[-1]
